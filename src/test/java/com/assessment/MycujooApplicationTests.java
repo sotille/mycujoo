9b5226379687ef1991e7558a0da40e4edb0b0281
Felipe Sotille <felipe.sotille@edge.pt>
2018-08-23 11:42:12 +0100

    library update, Readme update, firstTest

--- src/test/java/com/assessment/MycujooApplicationTests.java
+++ src/test/java/com/assessment/MycujooApplicationTests.java
@@ -1,11 +1,24 @@
 package com.assessment;
 
-import org.junit.Test;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
 public class MycujooApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void firstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+
+    @Test
+    public void secondTest() {
+        assertEquals(2, 1 + 1);
     }
 
 }
